0e0c72410d5d5d43dd6e52309c4d582d6cbaddfc
Vladislav <danirucakov@gmail.com>
2024-11-07 21:47:14 +0300

    fixed birthday message not showing when switching account and already shown message

--- src/components/layouts/header/sign-out-button.tsx
+++ src/components/layouts/header/sign-out-button.tsx
@@ -5,12 +5,15 @@ import { signOut } from "next-auth/react"
 import { Button } from "@/components/ui/button"
 
 export function SignOutButton() {
+  const onClick = async () => {
+    await signOut({ redirect: true, callbackUrl: "/" })
+
+    const currentYear = new Date().getFullYear()
+    localStorage.removeItem(`birthdayNotification-${currentYear}`)
+  }
+
   return (
-    <Button
-      variant="ghost"
-      size="sm"
-      onClick={() => signOut({ redirect: true, callbackUrl: "/" })}
-    >
+    <Button variant="ghost" size="sm" onClick={onClick}>
       Sign out
     </Button>
   )
